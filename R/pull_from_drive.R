5922c16cf3e0f070b2577f1334b2bf134766cfc3
andybeet <22455149+andybeet@users.noreply.github.com>
2020-04-28 17:22:11 -0400

    pull_from drive.r to pull files from google drive

--- R/pull_from_drive.R
+++ R/pull_from_drive.R
@@ -2,9 +2,9 @@
 #'
 #' Used for pulling archived Atlantis runs into the users workspace.
 #'
-#' @param id Character string. Name of task/scenario/id associated with output.
+#' @param idstr Character string. A file name/part of a file name or simply a string of characters associated with output filenames
 #' @param filePath Character string. Path to local Atlantis output directory. Files will be pulled to this directtory
-#' @param fileList Character vector. Files that need to be pulled from Google Drive. (Default = "all")
+#' @param fileList Character vector. Files that need to be pulled from Google Drive. (Default = "all"). Other options fileList = "string" pull all files that match \code{idstr}
 #' @param googledriveFolder Character String. Name of directory on google Drive to pull.
 #' @param rootid Drive-id. Atlantis root id on google drive. (Default id for NEFSC is bundled in this package)
 #'
@@ -16,7 +16,7 @@
 #'
 #'@export
 
-pull_from_drive <- function(id, pathToOutput=here::here("output"),fileList="all",googledriveFolder, rootid=atlantisdrive::rootid){
+pull_from_drive <- function(idstr, pathToOutput=here::here(),fileList="match",googledriveFolder, rootid=atlantisdrive::rootid){
 
   # list the folders
   atlantisFiles <- googledrive::drive_ls(rootid)
@@ -33,33 +33,39 @@ pull_from_drive <- function(id, pathToOutput=here::here("output"),fileList="all"
 
   } else { # ask if need to create folder
     message(paste0("\n Folder: ",googledriveFolder, "... NOT found."))
-
+    return(NULL)
   }
 
-  # Push files to folder. check if already present. overwrite?
-  # store list of files not pushed
-  filesNotPushed <- NULL
-  for (afile in fileList) {
-    uploadFile <- file.path(pathToOutput,afile)
-    newName <- paste0(id,"_",afile)
-    result <- tryCatch(
-      {
-        googledrive::drive_upload(uploadFile, path = atlantisid, name = newName, overwrite = overwrite)
-        res <- TRUE
-      },
-      error = function(e){
-        message(afile," already exists for task ",taskid)
-        return(FALSE)
-      } ,
-      warning = function(w) return(FALSE)
-    )
-    if (!result) {
-      filesNotPushed <- c(filesNotPushed,afile)
+  # Pull files from drive.
+  # store list of files not present on drive
+  allFiles <- googledrive::drive_ls(atlantisid)
+
+  if (length(fileList) > 1) { # fileList will be a list of files
+
+    for (afile in fileList) {
+      filenameDribble <- allFiles %>% dplyr::filter(grepl(pattern=afile,name))
+      filename <- filenameDribble$name
+      fid <- filenameDribble$id
+
+      googledrive::drive_download(googledrive::as_id(fid),path=file.path(pathToOutput,filename),overwrite=T)
+    }
+
+  } else if ((length(fileList) == 1) & (fileList == "match")) {
+    # pull everyhing with id
+    filesToPull <- allFiles %>% dplyr::filter(grepl(pattern=idstr,name))
+
+    for (fid in filesToPull$id) {
+      filename <- filesToPull %>%
+        dplyr::filter(id==fid) %>%
+        dplyr::select(name) %>%
+        unlist()
+
+      googledrive::drive_download(googledrive::as_id(fid),path=file.path(pathToOutput,filename),overwrite=T)
     }
   }
 
-  # set permission on files. Maybe unneccessary
 
-  return(filesNotPushed)
+return()
+
 
 }
