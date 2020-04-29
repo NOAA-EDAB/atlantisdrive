4d7fed9809a22a3de082c7763b716dfd82a05f7e
andybeet <22455149+andybeet@users.noreply.github.com>
2020-04-29 13:59:28 -0400

    pull_from_drive.R added examples

--- R/pull_from_drive.R
+++ R/pull_from_drive.R
@@ -2,7 +2,7 @@
 #'
 #' Used for pulling archived Atlantis runs into the users workspace.
 #'
-#' @param filePath Character string. Path to local Atlantis output directory. Files will be pulled to this directtory
+#' @param localPath Character string. Path to local Atlantis output directory. Files will be pulled to this directtory. Default = here::here()
 #' @param fileList Character vector. Files name(s) that need to be pulled from Google Drive. Alternatively any character string present in file name
 #' @param googledriveFolder Character String. Name of directory on google Drive to pull.
 #' @param rootid Drive-id. Atlantis root id on google drive. (Default id for NEFSC is bundled in this package)
@@ -13,20 +13,24 @@
 #'
 #' @return Files are pulled to local drive
 #'
+#'@examples
+#'
+#'\dontrun{
+#'# Pulls all xml files from the Development Folder to your current working directory
+#'pull_from_drive(fileList="xml",googledriveFolder="Development")
+#'
+#'# Pulls all files with the string "ATLNTS-2" present in its filename to the "output" folder in your project
+#'pull_from_drive(localPath = here::here("output"),fileList="ATLNTS-2",googledriveFolder="Development")
+#'
+#'# Pulls selected files from the Scenario Folder
+#' filesToPull <- c("file1.jpg","file2.txt","file3.bmg")
+#'pull_from_drive(fileList=filesToPull,googledriveFolder="Scenario")
+#'
+#'}
+#'
 #'@export
 
-pull_from_drive <- function(pathToOutput=here::here(),fileList,googledriveFolder=NULL, rootid=atlantisdrive::rootid){
-
-  # Error checks ------------------------------------------------------------
-  # if (length(fileList) <=1)
-  #   if ((length(fileList) == 1) & (fileList == "match"))
-  #     if(is.na(idstr))
-  #       stop("if fileList = \"match\" then idstr can not be NA")
-
-  # googledrive folder cannot be empty\
-  if (is.null(googledriveFolder))
-    stop("You must specify a folder on google drive in which to pull files from. See googleDriveFolder argument")
-
+pull_from_drive <- function(localPath=here::here(),fileList,googledriveFolder,rootid=atlantisdrive::rootid){
 
   # Detect folders ----------------------------------------------------------
 
@@ -61,7 +65,7 @@ pull_from_drive <- function(pathToOutput=here::here(),fileList,googledriveFolder
       filename <- filenameDribble$name
       fid <- filenameDribble$id
 
-      googledrive::drive_download(googledrive::as_id(fid),path=file.path(pathToOutput,filename),overwrite=T)
+      googledrive::drive_download(googledrive::as_id(fid),path=file.path(localPath,filename),overwrite=T)
     }
 
    } else if (length(fileList) == 1) {
@@ -74,7 +78,7 @@ pull_from_drive <- function(pathToOutput=here::here(),fileList,googledriveFolder
         dplyr::select(name) %>%
         unlist()
 
-      googledrive::drive_download(googledrive::as_id(fid),path=file.path(pathToOutput,filename),overwrite=T)
+      googledrive::drive_download(googledrive::as_id(fid),path=file.path(localPath,filename),overwrite=T)
     }
   }
 
