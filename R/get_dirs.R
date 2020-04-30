35e676548ca1abb1d8abf43b86cb84a09b750a8d
andybeet <22455149+andybeet@users.noreply.github.com>
2020-04-30 12:13:15 -0400

    Two new functions - get_dirs. pulls list of directories under the root dir - get_files. pulls list of files in a directory under the root

---
+++ R/get_dirs.R
@@ -0,0 +1,19 @@
+#' Get list of directories on google drive
+#'
+#' Lists all folders under the root google drive folder
+#'
+#' @param rootid Drive-id. Atlantis root id on google drive. (Default id for NEFSC is bundled in this package
+#'
+#' @return A tibble containing folder names, id, etc
+#'
+#' @family atlantisdrive functions
+#'
+#' @export
+
+get_dirs <- function(rootid=atlantisdrive::rootid){
+
+  dirs <- googledrive::drive_ls(googledrive::as_id(rootid))
+
+  return(dirs)
+
+}
--- R/get_files.R
+++ R/get_files.R
@@ -1,22 +1,28 @@
-#' Create a list of Core files used in Atlantis
+#' Get list of files with a diresctory on google drive
 #'
-#' Compiles a list of file names based on file extension (e.g. nc, txt, ts, xml, bgm)
+#' Lists all files under a directory on google drive folder
 #'
-#' @param path Character string. Path to location of atlantis output files
-#' @param filesToInclude Character vector. Types of files to copy (Default = ("txt","nc","ts","xml","bgm"))
+#' @return A tibble containing file name, id etc.
 #'
-#' @return A character vector containing the names of all of the files to be copied to the google drive
+#' @importFrom magrittr "%>%"
+#'
+#' @family atlantisdrive functions
 #'
 #' @export
 
-get_files <- function(path= here::here(),filesToInclude = c("txt","nc","ts","xml","bgm")){
+get_files <- function(rootid=atlantisdrive::rootid,targetDir){
 
-    fileList <- NULL
-    for (aext in filesToInclude) {
-      files <- list.files(path,paste0("*.",aext))
-      fileList <- c(fileList,files)
-    }
+  dirs <- get_dirs(rootid)
 
-  return(fileList)
+  selectedDir <- dirs %>% dplyr::filter(grepl(pattern=targetDir,name))
+
+  if (nrow(selectedDir) > 1) {
+    message(paste(selectedDir$name,collapse=","))
+    stop("Please refine targetDir. Multiple directories found")
+  }
+
+  files <- googledrive::drive_ls(googledrive::as_id(selectedDir$id))
+
+  return(files)
 
 }
