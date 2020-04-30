8efe58e9e8d97462eece413519ee2cf5b3c057c2
andybeet <22455149+andybeet@users.noreply.github.com>
2020-04-30 12:14:28 -0400

    list_core_files.R - lists set of Atlantis core output files

---
+++ R/list_core_files.R
@@ -0,0 +1,24 @@
+#' List of Core files used in Atlantis
+#'
+#' Compiles a list of file names based on file extension (e.g. nc, txt, ts, xml, bgm)
+#'
+#' @param path Character string. Path to location of atlantis output files
+#' @param filesToInclude Character vector. Types of files to copy (Default = ("txt","nc","ts","xml","bgm"))
+#'
+#' @return A character vector containing the names of all of the files to be copied to the google drive
+#'
+#' @family googldrive functions
+#'
+#' @export
+
+list_core_files <- function(path= here::here(),filesToInclude = c("txt","nc","ts","xml","bgm")){
+
+    fileList <- NULL
+    for (aext in filesToInclude) {
+      files <- list.files(path,paste0("*.",aext))
+      fileList <- c(fileList,files)
+    }
+
+  return(fileList)
+
+}
